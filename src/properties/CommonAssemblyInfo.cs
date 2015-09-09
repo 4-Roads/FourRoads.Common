668f5b19256893491a891593c007a2b7bed6f45b
Gary McPherson <genyus@gmail.com>
2015-09-09 01:36:35 +0100

    Bumped version to 3.3

--- src/properties/CommonAssemblyInfo.cs
+++ src/properties/CommonAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Four Roads, LLC 2007-2014")]
 [assembly: AssemblyTrademark("All rights reserved.")]
 // AssemblyVersion format: major.minor.0.0, e.g. 1.5.0.0
-[assembly: AssemblyVersion("3.2.0.0")]
+[assembly: AssemblyVersion("3.3.0.0")]
 // AssemblyFileVersion format: major.minor.patch.0, e.g. 1.5.3.0. Final file version will be overridden by CI server, e.g. 1.5.3.4596
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyFileVersion("3.3.0.0")]
 // AssemblyInformationalVersion format: As AssemblyFileVersion for CI builds or SemVer for production builds, e.g. major.minor.patch[-prereleasetag], e.g. 1.5.3-rc1
-[assembly: AssemblyInformationalVersion("3.2.0.0")]
+[assembly: AssemblyInformationalVersion("3.3.0.0")]
